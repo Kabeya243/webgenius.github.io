ace91a72823b8d541a81d88ba89d6809578acb17
Kabeya243 <kabeyailunga243@gmail.com>
2023-04-13 10:45:37 +0200

    add cdnjs links

---
+++ dist/script.js
@@ -0,0 +1,35 @@
+AOS.init();
+let closeBtn = document.querySelector('.close-btn');
+let openBtn = document.querySelector('.open-btn');
+let menu = document.querySelector('.menu');
+let links = document.querySelectorAll('.l');
+let body = document.body;
+let themeBtn = document.querySelector(".bxs-moon");
+
+// open and close menu script 
+openBtn.addEventListener('click',()=>{
+    menu.style.marginRight = '0px';
+    menu.style.transition = '.3s';
+    body.style.overflow = "hidden"
+    menu.style.display = "flex"
+})
+closeBtn.addEventListener('click',()=>{
+    menu.style.marginRight = '-400px';
+    menu.style.transition = '.3s';
+    body.style.overflowY = ""
+    menu.style.display = "none"
+})
+links.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        menu.style.transition = '.3s';
+        menu.style.overflowY = ""
+    })}
+)
+// open and close menu script
+// changing theme script 
+themeBtn.addEventListener('click',()=>{
+    body.classList.toggle('dark');
+    themeBtn.classList.toggle("bxs-sun");
+    closeBtn.style.color = "#f1f1f1"
+})
+// changing theme script 
